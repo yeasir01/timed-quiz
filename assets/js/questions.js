e4f963a67ae15658b25752ac815c5868af30c5de
Yeasir H <yeasir01@gmail.com>
2020-01-25 13:12:12 -0800

    built quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+var questions = [{
+        title: "What temperature does water boil at?",
+        choices: ["100c", "150C", "200c", "212c"],
+        answer: "100c"
+    },
+    {
+        title: "What is often seen as the smallest unit of memory",
+        choices: ["Kilobyte", "Megabyte", "Byte", "Gigabyte"],
+        answer: "Byte"
+    },
+    {
+        title: "Bangladesh is bordered on the West by which country?",
+        choices: ["Russia", "Iran", "China", "India"],
+        answer: "Inda"
+    },
+    {
+        title: "The National Baseball Hall of Fame is located in which U.S. state?",
+        choices: ["California", "Texas", "New York", "Florida"],
+        answer: "New York"
+    },
+    {
+        title: "Which mammal is known to be the fastest land animal?",
+        choices: ["Cheetah", "Lion", "Greyhound", "Leopard"],
+        answer: "Cheetah"
+    },
+    {
+        title: "How many grams are there in a kilogram?",
+        choices: ["950", "500", "1,000", "2,000"],
+        answer: "1,000"
+    },
+    {
+        title: "Which is the highest mountain in Africa?",
+        choices: ["Mount Everest", "Mount Kilimanjaro", "Mount Fuji", "Mount Olympus"],
+        answer: "Mount Kilimanjaro"
+    },
+    {
+        title: "In which year was Alaska sold to the U.S.?",
+        choices: ["1867", "1901", "1888", "1890"],
+        answer: "1867"
+    },
+    {
+        title: "In the Star Wars universe, who is Luke Skywalker’s mother?",
+        choices: ["Mon Mothma", "Rose Tico", "Queen Apailana", "Padmé Amidala"],
+        answer: "Padmé Amidala"
+    },
+    {
+        title: "Which fast food franchise has the most worldwide locations?",
+        choices: ["Burger King", "Mcdonalds", "Subway", "KFC"],
+        answer: "Subway"
+    },
+];
+
+
+/* 
+HOW TO SETUP ARRAY
+ var anyNameHere ['question here', choices: ['option1', 'option2', 'option3'], answer: 'correct answer here']
+ */
