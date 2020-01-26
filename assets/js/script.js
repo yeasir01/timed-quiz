aebe402843564c9f5fb5b5cbab9dffe673a5308e
Yeasir H <yeasir01@gmail.com>
2020-01-25 21:50:57 -0800

    added functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//Get elements from HTML Document & Intialize
+const startWindowEl = document.getElementById('start-panel')
+const gameWindowEl = document.getElementById('game-panel')
 const resultEl = document.getElementById('result');
 const questionEl = document.getElementById('question');
 const timerEl = document.getElementById('timer');
@@ -6,5 +9,50 @@ const answrOneEl = document.getElementById('a1');
 const answrTwoEl = document.getElementById('a2');
 const answrThreeEl = document.getElementById('a3');
 const answrFourEl = document.getElementById('a4');
+const startEl = document.getElementById('start-btn')
 const highScoreEl = document.getElementById('high-btn');
 const restartEl = document.getElementById('restart-btn');
+
+var time = 60;
+var intitalCount = 0;
+
+//Button event listeners
+startEl.addEventListener('click', startGame)
+highScoreEl.addEventListener('click', highScores)
+restartEl.addEventListener('click', restart)
+
+
+function startGame() {
+    startWindowEl.className += ' ' + 'hide';  //takes exsisting classes & adds [space + "hide"]
+    gameWindowEl.classList.remove('hide');   //removes ["hide"] from class list
+    startTimer()
+}
+
+function skipQuestion(){
+    
+}
+
+function selectAnwser(){
+
+}
+
+function highScores(){
+    
+}
+
+function restart(){
+    gameWindowEl.className += ' ' + 'hide';  //takes exsisting classes & adds [space + "hide"]
+    startWindowEl.classList.remove('hide');   //removes ["hide"] from class list
+}
+
+function startTimer(){
+    timerEl.innerHTML = 2 + ":" + 50;   //Set intital time & format
+}
+
+function correctAnwser(){
+    resultEl.innerHTML += '<i class="far fa-check-circle correct"></i>';
+}
+
+function wrongAnwser(){
+    resultEl.innerHTML += '<i class="far fa-times-circle wrong"></i>';
+}
