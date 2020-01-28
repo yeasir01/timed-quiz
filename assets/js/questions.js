b4b56956471c49d91cff8839a00aaf0d3a4263f5
Yeasir H <yeasir01@gmail.com>
2020-01-27 18:26:07 -0800

    modified answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,57 +1,57 @@
 var questions = [{
         title: "What temperature does water boil at?",
         choices: ["100c", "150C", "200c", "212c"],
-        answer: "100c"
+        answer: "a"
     },
     {
         title: "What is often seen as the smallest unit of memory",
         choices: ["Kilobyte", "Megabyte", "Byte", "Gigabyte"],
-        answer: "Byte"
+        answer: "c"
     },
     {
         title: "Bangladesh is bordered on the West by which country?",
         choices: ["Russia", "Iran", "China", "India"],
-        answer: "Inda"
+        answer: "d"
     },
     {
         title: "The National Baseball Hall of Fame is located in which U.S. state?",
         choices: ["California", "Texas", "New York", "Florida"],
-        answer: "New York"
+        answer: "c"
     },
     {
         title: "Which mammal is known to be the fastest land animal?",
         choices: ["Cheetah", "Lion", "Greyhound", "Leopard"],
-        answer: "Cheetah"
+        answer: "a"
     },
     {
         title: "How many grams are there in a kilogram?",
         choices: ["950", "500", "1,000", "2,000"],
-        answer: "1,000"
+        answer: "c"
     },
     {
         title: "Which is the highest mountain in Africa?",
         choices: ["Mount Everest", "Mount Kilimanjaro", "Mount Fuji", "Mount Olympus"],
-        answer: "Mount Kilimanjaro"
+        answer: "b"
     },
     {
         title: "In which year was Alaska sold to the U.S.?",
         choices: ["1867", "1901", "1888", "1890"],
-        answer: "1867"
+        answer: "a"
     },
     {
         title: "In the Star Wars universe, who is Luke Skywalker’s mother?",
         choices: ["Mon Mothma", "Rose Tico", "Queen Apailana", "Padmé Amidala"],
-        answer: "Padmé Amidala"
+        answer: "d"
     },
     {
         title: "Which fast food franchise has the most worldwide locations?",
-        choices: ["Burger King", "Mcdonalds", "Subway", "KFC"],
-        answer: "Subway"
+        choices: ["Burger King", "McDonalds", "Subway", "KFC"],
+        answer: "c"
     },
 ];
 
 
 /* 
-HOW TO SETUP ARRAY
+HOW TO SETUP ARRAY FOR Q&A
  var anyNameHere ['question here', choices: ['option1', 'option2', 'option3'], answer: 'correct answer here']
  */
