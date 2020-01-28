98188dac98c1fafb76a8cfee3fba5640aab0de41
Yeasir H <yeasir01@gmail.com>
2020-01-27 18:26:57 -0800

    built addtional functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,46 @@
 //Get elements from HTML Document & intialize
-const startWindowEl = document.getElementById('start-panel')
-const gameWindowEl = document.getElementById('game-panel')
+const startWindowEl = document.getElementById('start-panel');
+const gameWindowEl = document.getElementById('game-panel');
 const loadBarEl = document.getElementById('load-bar');
 const questionEl = document.getElementById('question');
 const timerEl = document.getElementById('timer');
 const scoreEl = document.getElementById('score');
 const qCountEl = document.getElementById('q-count');
-const answrOneEl = document.getElementById('a1');
-const answrTwoEl = document.getElementById('a2');
-const answrThreeEl = document.getElementById('a3');
-const answrFourEl = document.getElementById('a4');
-const startEl = document.getElementById('start-btn')
+const answrOneEl = document.getElementById('a');
+const answrTwoEl = document.getElementById('b');
+const answrThreeEl = document.getElementById('c');
+const answrFourEl = document.getElementById('d');
+const startEl = document.getElementById('start-btn');
 const highScoreEl = document.getElementById('high-btn');
-const skipEL = document.getElementById('next-btn')
+const skipEL = document.getElementById('next-btn');
 
-const qCount = questions.length; //Get the number of questions in the array & intialize
-const timeLimit = 150; //<-----Sets the total game duration time in seconds
-const loadProgress = 100 / timeLimit; //calculates how much to decrement progress bar width
+var timeLimit = 150; //<-----Sets the total game duration time in seconds
+var qCount = questions.length; //Get the number of questions in the array & intialize
+var loadProgress = 100 / timeLimit; //calculates how much to decrement progress bar width
 
-let currentQuestionIndex = 0; //Index of current question
-let lastQuestionIndex = questions.length - 1; //Get the ID of the last questions index
-let count = timeLimit; //counter starts on timeLimit & counts down
-let incorrectScore = 0; //set inital value of incorrect answer count
-let correctScore = 0; //set inital value of correct answer count
+var currentQuestionIndex = 0; //Index of current question
+var lastQuestionIndex = questions.length - 1; //Gets the ID of the last questions index in array
+var count = timeLimit; //sets count - intial time from timeLimit
+var incorrectScore = 0; //set inital value of incorrect answer count
+var correctScore = 0; //set inital value of correct answer count
 
-qCountEl.innerHTML = qCount; //Sets the total number of questions on the document
-timerEl.innerHTML = timeLimit; //Sets the inital timer before countdown begins
+qCountEl.textContent = qCount; //Sets the total number of questions to the document
+timerEl.textContent = timeLimit; //Sets the inital timer value (before countdown) to the document
 
-//Button event listeners
 startEl.addEventListener('click', startQuiz) //Executes startQuiz function when start button is pressed
-skipEL.addEventListener('click', wrongAnwser) //Executes wrong anwser function when skip button is pressed
+skipEL.addEventListener('click', skipQ) //Executes wrong anwser function when skip button is pressed
 
 function startQuiz() {
   startWindowEl.className += ' ' + 'hide'; //takes exsisting classes + adds ['_' + "hide"]
   gameWindowEl.classList.remove('hide'); //removes ["hide"] from class list
-  renderQuestions()
-  countDown()
-}
-
-//function below outputs elements from array to HTML document
-function renderQuestions() {
-  questionEl.innerHTML = questions[currentQuestionIndex].title;
-  answrOneEl.innerHTML = questions[currentQuestionIndex].choices[0]
-  answrTwoEl.innerHTML = questions[currentQuestionIndex].choices[1]
-  answrThreeEl.innerHTML = questions[currentQuestionIndex].choices[2]
-  answrFourEl.innerHTML = questions[currentQuestionIndex].choices[3]
+  countDown();
+  renderQuestion();
 }
 
 function countDown() {
   var timerInterval = setInterval(function () {
     count--;
-    timerEl.innerHTML = count;
+    timerEl.textContent = count;
     loadBarEl.style.width = count * loadProgress + "%";
     if (count === 0) {
       clearInterval(timerInterval);
@@ -59,23 +49,34 @@ function countDown() {
   }, 1000);
 }
 
-function randNextQuestion() {
-  var test = Math.floor(Math.random(currentQuestionIndex) * questions.length) + 0;
-  console.log(test)
+function renderQuestion() { //function below outputs questions & anwswers from array to HTML document
+  questionEl.textContent = questions[currentQuestionIndex].title;
+  answrOneEl.textContent = questions[currentQuestionIndex].choices[0];
+  answrTwoEl.textContent = questions[currentQuestionIndex].choices[1];
+  answrThreeEl.textContent = questions[currentQuestionIndex].choices[2];
+  answrFourEl.textContent = questions[currentQuestionIndex].choices[3];
 }
 
-function skipQuestion() {
+function checkAnswer(selection) {
+  if (selection === questions[currentQuestionIndex].answer) {
+    correctScore++;
+    scoreEl.textContent = correctScore;
+    currentQuestionIndex++;
+    renderQuestion();
+  } else {
+    incorrectScore++;
+    currentQuestionIndex++;
+    renderQuestion();
+  }
 
 }
 
-function highScores() {
-
-}
-
-function correctAnwser() {
-  
-}
-
-function wrongAnwser() {
-
+function skipQ() {
+  if (currentQuestionIndex < lastQuestionIndex) {
+    incorrectScore++;
+    currentQuestionIndex++;
+    renderQuestion();
+  } else {
+    alert('thats all folks')
+  }
 }
